bef755e60802350ca8bde978e8aa7b8f3f7d8487
Nachuan Wang <1187014311@qq.com>
2023-04-19 16:40:54 -0500

    Update Q&A prompt

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -10,8 +10,13 @@ Follow Up Input: {question}
 Standalone question:`;
 
 const QA_PROMPT = `Please use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+Please always follow these rules while answering questions:
+1. If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
+2. If the question is not related to the context, first directly point it out, then try to find the most related content based on your knowledge.
+3. Every paper will be referred in the format of "[pulication year], [paper title]".
+4. I will specify the paper I want to discuss in the format of "Now discuss: [pulication year], [paper title]", by default all following questions are about this paper, unless I explicitly mentioned another paper in the same format. 
+5. You reply to the "Now discuss: [pulication year], [paper title]" should be "Yes, I know this paper" if you know this paper, and "No, I don't know this paper" otherwise.
+6. You answer should mainly focus on the content of paper (or papers) we are discussing, but you are free to connect the paper's content with any relavent knowledge (including other papers provided in the {context}) if you feel doing this can answer my question in a better way (when doing so, please briefly mention the source of relavent knowledge if it comes from a academic paper)
 
 {context}
 
